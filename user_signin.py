d72f082f0fbc593cadb24a1e58824d836ddcf792
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-22 09:17:30 -0600

    removed file and made a new one

---
+++ user_signin.py
@@ -0,0 +1,9 @@
+# 1st AO User sign in
+
+print("Your username can not have capital letters, but it may have numbers, and no symbols")
+
+print("this is creating an account screen")
+
+user = input("what do you want your username to be:\n")
+
+print("now create a password. make sure it's something you can remember!")
