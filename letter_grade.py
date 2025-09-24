6a643fd486dc8fb98961a4ad1fc9ff468070e51c
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-24 09:51:48 -0600

    made new code

---
+++ letter_grade.py
@@ -0,0 +1,19 @@
+# AO 1st CP1
+
+
+grade = float(input("what is your grade:\n"))
+
+
+
+
+if grade >= 92:
+    print("congrats! You have an A. Now keep that grade and don't let it go down")
+elif grade > 85 and grade < 92:
+    print("you have a B+ that's pretty good, now I know you can do better! Push yourself ")
+elif grade >= 83 and grade <= 85:
+    print("You got a B, you can do better, but pretty good so far!")
+elif grade > 80 and grade < 82:
+    print("you have a B- congrats! You're doing pretty well, but you can do better for sure. ")
+
+
+
