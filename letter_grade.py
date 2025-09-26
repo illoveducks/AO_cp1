ba5a5ce1864332836ddb6451234ec390c93af8dc
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-26 09:29:22 -0600

    added and fixed

--- letter_grade.py
+++ letter_grade.py
@@ -16,5 +16,17 @@ elif grade > 80 and grade < 82:
     print("you have a B- congrats! You're doing pretty well, but you can do better for sure. ")
 elif grade >= 76 and grade < 80:
     print(" You have a C+, you can do better. ")
+elif grade < 74 and grade > 72:
+    print("you have a C, do better. ")
+elif grade > 70 and grade < 72:
+    print("You got a C-, no words")
+elif grade > 66 and grade < 70:
+    print("A D+, do better")
+elif grade > 64 and grade < 66:
+    print("D, ouch, you can do better")
+elif grade > 60 and grade < 64:
+    print(" A D... Really?")
+else:
+    print(" You're a failure, that's what an F is, you should be in a different class, or do better.")
 
 
