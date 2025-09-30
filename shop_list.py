0e37e15444f736e605ce72c9794e9153a4df4d94
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-30 09:16:18 -0600

    hi

---
+++ shop_list.py
@@ -0,0 +1,10 @@
+# AO class Shopping List Manager
+
+shop_list = []
+
+while True:
+    action = #user input with instructions
+    #Write your code here
+    
+    elif action == "exit":
+    break
