3e5c4cd911660f31f73a5a5654616a90a5824d36
illoveducks <airam.ortiz@ucas-edu.net>
2025-10-01 09:08:49 -0600

    code

--- shop_list.py
+++ shop_list.py
@@ -3,8 +3,24 @@
 shop_list = []
 
 while True:
-    action = #user input with instructions
-    #Write your code here
-    
-    elif action == "exit":
-    break
+    print("to add things type1, to remove things type 2, to show the list type 3, to quit type 4")
+    action = input("please type a number 1-4:\n")
+   
+   
+    if action == "1":
+        item = input("Add item:\n")
+        shop_list.append(item)
+        print(f"{item} added.")
+    elif action == "2":
+        item = input(" remove item:\n")
+        if item in shop_list:
+            shop_list.remove(item)
+            print(f"{item} removed.")
+        else:
+            print("this item is not on your list.")
+    elif action == "3":
+        print(shop_list)
+        
+    elif action == "4":
+        print("goodbye!")
+        break
