6f7906254634521c317a778d63d175b9a87643e1
illoveducks <airam.ortiz@ucas-edu.net>
2025-10-02 09:52:49 -0600

    multiplication table

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,7 +1,17 @@
 # AO 1st multiplication table CP1
 
+multi_table = [
+              []
+
+              ]
+
+
+
 one = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-
-for num1 in one:
+class range():
+    for num1 in one:
+        num1 == range(1, 12, 1)
+        multi_table.append(num1)
+        print(multi_table, )
+        
     
