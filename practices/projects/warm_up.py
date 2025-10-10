042e6936f9de28bcd98f8b6e7b3b287d1ca2a929
illoveducks <airam.ortiz@ucas-edu.net>
2025-10-10 09:52:56 -0600

    new code

---
+++ practices/projects/warm_up.py
@@ -0,0 +1,27 @@
+
+
+
+import random
+
+numbers =   [0, 1, 2,
+             3, 4, 5,
+             6, 7, 8 ] 
+
+
+while True:
+
+    player = int(input("give me a number 0-8"))
+
+    if numbers(player)
+
+    computer = random.randint(0,8)
+
+
+
+    if player == [1] [2] [3]:
+        print("you win!")
+    elif player == [4] [5] [6]:
+        print("you win!")
+    elif player == [7] [8] [9]:
+    
+    elif player == [1] [5] [9]:
