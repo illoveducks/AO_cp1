0fe4e20d6ae189ca29c0594ca4d8a58d945df8d1
illoveducks <airam.ortiz@ucas-edu.net>
2025-10-22 09:52:54 -0600

    comments a lot of them

---
+++ practices/projects/caesar_cipher.py
@@ -0,0 +1,22 @@
+# AO 1st ceasar cipher encoder and decoder
+
+
+
+
+
+
+# Stuff I don't get!
+# looking at instructions till I understand 
+
+
+# ask user for a phrase and shift amount
+# there will be two alphabets, one capital on lower
+# the shift amount adds to index which allows numbers to shift
+# when the index is over 25, reset it to one, so z turns into 1 which will be a,
+# same with the captals 
+
+
+import re
+
+
+
