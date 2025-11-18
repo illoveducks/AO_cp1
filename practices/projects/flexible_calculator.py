72f242616342c93ddd4683087600dc50e51669e3
illoveducks <airam.ortiz@ucas-edu.net>
2025-11-18 09:52:40 -0700

    uhhhhhhhhhhhhhhhhhhhh...?

--- practices/projects/flexible_calculator.py
+++ practices/projects/flexible_calculator.py
@@ -1,3 +1,44 @@
 # AO 1st Flexible calculator calculator
 
-print("Hi")
+# introduce user to the program
+print("welcome to my flexible calculator!")
+
+print("pick an operation! ")
+
+# just for stupid proof
+while True:
+    # ask user for what equation they want
+    equation = input("sum1, average2, max3, min4, product5, please type 1-5:\n ")
+    
+    # Setting the operation
+
+    if equation == "1":
+        print("alright addition, now I need numbers!")
+    # write down process in here and others
+    # after user finishes writing their numbers will calculate the sum 
+
+            num =  input.append(numbers("what number would you like to add? type ' done' when you're done" ))
+
+
+        
+
+
+    if equation == "2":
+        print("alright average, now I need numbers!")
+
+
+
+    if equation == "3":  
+        print("alright max, now I need numbers!")
+
+
+
+
+    if equation == "4":
+        print("alright addition, now I need numbers!")
+
+
+    if equation == "5":
+        print("product, now I need numbers!")
+
+   
