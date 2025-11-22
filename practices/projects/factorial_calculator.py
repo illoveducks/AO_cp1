38a35a4a36224a7a87100e71aa325542ccfa0bb2
illoveducks <airam.ortiz@ucas-edu.net>
2025-11-22 20:45:56 +0000

    sdhufhudfbgf,jvhhj

--- practices/projects/factorial_calculator.py
+++ practices/projects/factorial_calculator.py
@@ -9,29 +9,42 @@
 # else:
 # for in rnage (num, choice):
     # Multiply * choice
-# c= display original number and i 
-# return c
-num = 1 
-# while true
-# function on numbers() uh... what?
+    # c= display original number and i 
+    # return c
 
+    # while true
+    # function on numbers() uh... what?
+
+
+num = 1
+
+
+print("pick a number that is not a negative or decimal ")
 
 while True:
 
-    print("pick a number that is not a negative or decimal ")
-
     choice = int(input("type a number \n "))
+ 
+
+
+
+        
 
     if choice < 0:
-        print("no numbers smaller than one")
+        print("no negatives ")
+    elif choice < 1:
+        print("no numbers smaller than one please! ")
+
+    elif choice == choice and choice > 1:
+        
+        for i in range(1, choice + 1):
+            num = num * i
+        print("original choice:", choice, "factorial: ", num)
+    break
+        
+                                              
 
 
-    for i in range(num, choice):
-        i * choice
-        print("original choice:", choice, "factorial: ", i)
-  
 
-
-
-  # there's a logic error, the code runs well, but it doesn't break
-  # The numbers are not multiplied
+                                                # there's a logic error, the code runs well, but it doesn't break
+                                                  # The
