2920ced9cb3bccc87af75ad07cd4388bc363db77
illoveducks <airam.ortiz@ucas-edu.net>
2025-11-25 14:45:31 -0700

    s;8oy8oy8y

---
+++ practices/projects/adventure_game.py
@@ -0,0 +1,10 @@
+# AO 1st adventure game pseudocode stage!
+
+# import random for certain enemy attacks, some might have a pattern ( could be changed  )
+# Import time so that everything doesn't appear at once, 
+#The game will start wow... By someone pressing that play button looking thing is VS code
+# The setting will begin with 'Mark' waking up at home
+# display " this feels famaliar, " wait a second display " make myself breakfast, bacon, eggs, like always"
+# wait a bit again... display " i thought to myself something feels different..."
+# Make the character walk around, user can choose whether to inspect, after a bit of inspecting, 
+# Display " that's what's different, there is no ketchup...The only day...Something is off. "
