f98f6c617f0930d24f4c7b20855087f17bdf93ff
illoveducks <airam.ortiz@ucas-edu.net>
2025-12-01 07:00:36 +0000

    little bit added to give me a start, Needs to be polished.

--- practices/projects/adventure_game.py
+++ practices/projects/adventure_game.py
@@ -1,10 +1,86 @@
 # AO 1st adventure game pseudocode stage!
 
 # import random for certain enemy attacks, some might have a pattern ( could be changed  )
+
 # Import time so that everything doesn't appear at once, 
-#The game will start wow... By someone pressing that play button looking thing is VS code
+
+# The game will start wow... By someone pressing that play button looking thing is VS code
+
+# Need a dictionary for items equipped I will need help on that
+# need a dictionary for stats
+
 # The setting will begin with 'Mark' waking up at home
+
 # display " this feels famaliar, " wait a second display " make myself breakfast, bacon, eggs, like always"
 # wait a bit again... display " i thought to myself something feels different..."
-# Make the character walk around, user can choose whether to inspect, after a bit of inspecting, 
-# Display " that's what's different, there is no ketchup...The only day...Something is off. "
+
+# wait a bit again, display " I look around, I seem to have forgotten everything, the world seems shattered..."
+
+# wait a bit again " I feel like someone is there, may we go outside?" User input " Yes " or " No "
+
+# if user chooses " yes ", Mark goes outside, to be greeted by his neighbor who's name he doesn't remember
+# display " Hey Mark! How's it going? You should head towards the forest, the weather is nice! "
+# wait a bit... display "... "
+# display "awfully quiet huh? Oh well! There's new bridges, one connects to the forest, a desert and a new town! Go visit those some time! See you... around "
+
+# ELSE IF user choose ' no '
+
+# Display " Let it be that way then... Do you want it to end? 
+# choice ' yes ', ' no '
+
+
+
+
+#  if user picks ' no ' Mark heads outside, to be greeted by his neighbor who's name he doesn't remember
+# display " Hey Mark! How's it going? You should head towards the forest, the weather is nice! "
+# wait a bit... display "... "
+# display "awfully quiet huh? Oh well! There's new bridges, one connects to the forest, a desert and a new town! Go visit those some time! See you... around "
+
+
+
+
+# ELSE if user says ' yes ' display Freedom... and Break
+
+# display I wish I had the strenght to talk... Where shall we go?
+# Break to add onto another loop so when it continues it doesn't go back to the beginning! 
+
+
+# While True:
+
+# user choice 'forest', 'desert', ' home ', 'new town' 
+
+# If user is not either of those, display " Really? Are you messing with me? " continue command 
+
+# And make a nested while true! If...That doesn't break it...
+# If user choice is ' forest '
+# Display " that's far... I'm willing to go... Far... Far... " wait a bit... 3 seconds. display " I'm here. "
+
+# display " Why did you bring me here...? There are sticks everywhere... "
+# user choice 'pick up' ( Stick ) , 'continue further', 'go back' 
+
+# If user choice ' pick up ' display "okay...? " add stick damage to damage stat and weapon equipeed in dictionary
+
+# display What now...? user choice ' pick up (stick)', ' continue further ', 'go back '
+# if user choice is pick up display" I'm not picking up another one... "
+# 
+# user choice ' continue further ', ' go back ' 
+# if user choice is continue further display " fine... " wait a bit... 4 seconds " i've reached...The end "
+# wait a bit, display " I see a wolf...Do you really want me to...? "
+# user choice ' fight ', ' leave it', ' head to the gate ',
+
+# if user choice is 'leave it' display " Thank you, what now?"
+# user choice ' fight ', ' head to the gate ' 
+
+# if user does fight goes to ELSE if
+
+# if user choice is ' head to the gate '
+# display " Fine... " wait a bit... display " The gate is closed... There's Something I need...Here...Here..."
+# a sign, it displays " The gate is a bridge to all biomes, you may head anywhere from here. " 
+
+
+
+# ELSE if user choice is fight Mark wakes the wolf up my hitting it with the stick. 
+# Wolf has 50 Hp, no armor, deals 5 damage with scratch, 8 with pounce and none with howl
+
+# Mark can block with his stick if he has one if block is succesful heal mark by 3 hp, stab with the stick deals 8 damage,
+# Mark has 20 HP, armor reduces damage, if you had gotten any before heading here
