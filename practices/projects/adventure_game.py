dfb5afbc726d110861fbae59b75fad2f1bc1c21b
illoveducks <airam.ortiz@ucas-edu.net>
2025-12-03 09:52:20 -0700

    kutfkutfukuyfku

--- practices/projects/adventure_game.py
+++ practices/projects/adventure_game.py
@@ -3,3 +3,28 @@
 import random 
 
 import time
+
+Marks_stats = { "damage": 5,
+               "health": 20,
+               "defense": 0, }
+
+Status = { "wolf": "alive",
+            "royal guard": 'alive',
+            "panda": ' alive',
+            "desert golem": 'alive?',
+            "program": 'unbroken'}
+
+
+Marks_location = "home"
+
+print("I woke up, I wonder, I wonder what's going on in town today. I need to get ready")
+
+time.sleep(1)
+
+print("I made breakfast, something is wrong, I feel it, I looked outside, to see a world ' shattered ', why?")
+
+time.sleep(1)
+
+print("After finishing breakfast, I got up, should we head outside?")
+
+while True:
